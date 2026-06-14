08d9137762a3c9ed229b9e87661b118846569aa6
Sachin-mr <sachin-mr@outlook.com>
2026-06-14 23:42:21 +0530

    implemented singleton pattern

---
+++ SingletonPatternExample/src/com/singleton/Test.java
@@ -0,0 +1,11 @@
+package com.singleton;
+
+public class Test {
+	public static void main(String[] args) {
+		Logger l1 = Logger.getInstance();
+		Logger l2 = Logger.getInstance();
+		
+		System.out.println(l1==l2);
+	}
+
+}
