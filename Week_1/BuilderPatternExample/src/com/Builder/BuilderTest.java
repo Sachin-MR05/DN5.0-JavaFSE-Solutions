6fa42ef90ac2d5495ac98db620d9baa64f0fc304
Sachin-mr <sachin-mr@outlook.com>
2026-06-15 11:28:10 +0530

    Implemented Builder Example

---
+++ Week_1/BuilderPatternExample/src/com/Builder/BuilderTest.java
@@ -0,0 +1,26 @@
+package com.Builder;
+
+public class BuilderTest {
+
+    public static void main(String[] args) {
+
+        Computer gamingPC = new Computer.Builder()
+                .setcpu("Intel i9")
+                .setRam(32)
+                .setStorage(1000)
+
+                .build();
+
+        Computer officePC = new Computer.Builder()
+                .setcpu("Intel i5")
+                .setRam(16)
+                .setStorage(512)
+                .build();
+
+        System.out.println("Gaming PC");
+        gamingPC.display();
+
+        System.out.println("Office PC");
+        officePC.display();
+    }
+}
