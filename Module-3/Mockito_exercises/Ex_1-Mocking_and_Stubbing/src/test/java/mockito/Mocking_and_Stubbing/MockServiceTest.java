f166cea56f4c3a4e6340a00240238d191bc8d0b7
Sachin-mr <sachin-mr@outlook.com>
2026-06-25 22:26:00 +0530

    inplemented mocking and stubing

--- Module-3/Mockito_exercises/Ex_1-Mocking_and_Stubbing/src/test/java/mockito/Mocking_and_Stubbing/MockServiceTest.java
+++ Module-3/Mockito_exercises/Ex_1-Mocking_and_Stubbing/src/test/java/mockito/Mocking_and_Stubbing/MockServiceTest.java
@@ -11,17 +11,14 @@ public class MockServiceTest {
 
     @Test
     public void testExternalApi() {
-        // Step 1: Create mock object
+
         ExternalApi mockApi = mock(ExternalApi.class);
 
-        // Step 2: Stub method
-        when(mockApi.getData()).thenReturn("Mock Data");
+        when(mockApi.getData()).thenReturn("I am Sachin");
 
-        // Step 3: Use mock in service
         MyService service = new MyService(mockApi);
         String result = service.fetchDataa();   // not fetchDataa()
 
-        // Step 4: Check output
-        assertEquals("Mock Data", result);
+        assertEquals("I am Sachin", result);
     }
 }
